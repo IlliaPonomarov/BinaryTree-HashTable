6ef26d03fedabf8a373c2c14d1d23583ec407345
= <pythonprogramatorqq@gmail.com>
2022-03-16 13:49:22 +0100

    getAll function was added to ChainHash

--- src/main/java/HashTables/ChainHash.java
+++ src/main/java/HashTables/ChainHash.java
@@ -1,11 +1,15 @@
 package HashTables;
 
+import org.w3c.dom.html.HTMLObjectElement;
+
+import java.util.*;
 import java.util.LinkedList;
 
 public class ChainHash {
-    final static int SIZE = 1000;
+    final static int CAPACITY = 1000;
 
-    LinkedList<HTObject>[] hashtable = new LinkedList[SIZE  +1];
+    public static LinkedList<HTObject>[] hashtable = new LinkedList[CAPACITY + 1];
+    List<Integer> allKeys = new ArrayList<Integer>();
 
     class HTObject {
         Integer key;
@@ -14,6 +18,7 @@ public class ChainHash {
         public HTObject(Integer key, String value) {
             this.key = key;
             this.value = value;
+            allKeys.add(key);
         }
 
         public Integer getKey() {
@@ -24,21 +29,22 @@ public class ChainHash {
             return value;
         }
 
-        public HTObject(){}
+        public HTObject() {
+        }
     }
 
     public ChainHash() {
-        for (int i = 0; i < SIZE + 1; i++)
+        for (int i = 0; i < CAPACITY + 1; i++)
             hashtable[i] = null;
     }
 
 
-    public int hashFunction(Integer key){
+    public int hashFunction(Integer key) {
         int hash = key.hashCode();
-        return (hash & 0x7FFFFFFF) % SIZE;
+        return (hash & 0x7FFFFFFF) % CAPACITY;
     }
 
-    public void insertHash(Integer key, String value){
+    public void insertHash(Integer key, String value) {
         int index = hashFunction(key);
 //        HTObject linkNext = new HTObject(key);
 //        linkNext.next = null;
@@ -65,60 +71,108 @@ public class ChainHash {
 //        return chainHash;
         LinkedList<HTObject> htObjects = hashtable[index];
 
-        if (htObjects == null){
+        if (htObjects == null) {
             htObjects = new LinkedList<HTObject>();
             htObjects.add(new HTObject(key, value));
             hashtable[index] = htObjects;
-        }
-        else{
+        } else {
             for (int i = 0; i < htObjects.size(); i++) {
 
-                if (hashtable.length >= )
+                if (hashtable.length == CAPACITY)
+                    hashtable = resize();
 
-                if (htObjects.get(i).key.equals(key))
-                    htObjects.get(i).value = value;return;
+                if (htObjects.get(i).getKey().equals(key)) {
+                    htObjects.add(new HTObject(key, value));
+                    return;
+                }
             }
             htObjects.add(new HTObject(key, value));
             hashtable[index] = htObjects;
+
         }
     }
 
-    public String deleteByKey(int key){
+    public String deleteByKey(int key) {
 
         int index = hashFunction(key);
-        if (hashtable != null){
+        if (hashtable != null) {
             LinkedList<HTObject> htObjects = hashtable[index];
             for (int i = 0; i < htObjects.size(); i++)
                 if (htObjects.get(i).key.equals(key)) {
-                    htObjects.get(i).key = null;
-                    htObjects.get(i).value = null;
+                    htObjects.remove(htObjects.get(i));
                     System.out.println("Delete was successful");
                 }
-        }else
+        } else
             return null;
         return null;
     }
 
-    public String getValueByKey(int key){
+    public String getValueByKey(int key) {
 
         int index = hashFunction(key);
         if (hashtable != null) {
 
             LinkedList<HTObject> htObjectLinkedList = hashtable[index];
             for (int i = 0; i < htObjectLinkedList.size(); i++) {
-                if (htObjectLinkedList.get(i).key.equals(key))
-                    return htObjectLinkedList.get(i).value;
+                if (htObjectLinkedList.get(i).key.equals(key)) {
+                    System.out.println(htObjectLinkedList.get(i).value);
+                }
             }
         }
 
         return null;
     }
 
-    public void  resize(){
-        LinkedList<HTObject>[] newList = new LinkedList[]{};
+    public LinkedList<HTObject>[] resize() {
+        LinkedList<HTObject>[] newList = new LinkedList[CAPACITY * 2];
+
+        Integer key = 0, index = 0;
+        for (int i = 0; i < CAPACITY * 2; i++)
+            newList[i] = null;
+
+        for (int i = 0; i < CAPACITY; i++) {
+            if (hashtable[i] == null)
+                continue;
+
+            else {
+                for (int j = 0; j < hashtable.length; j++) {
+                    if (hashtable[j] != null) {
+
+                        key = hashtable[j].get(0).getKey();
+                        key = key.hashCode();
+                        index = key % (CAPACITY * 2);
+
+                        insertHash(key, hashtable[j].get(j).getValue());
+                    }
+
+                }
+            }
+        }
+
+
+        return newList;
+    }
+
+    public void getAll() {
+
+        LinkedList<HTObject> linkedList = null;
+        int index  = 0;
+        int i  = 0;
+        for (LinkedList<HTObject> objects: hashtable) {
+            if (objects != null) {
+                System.out.println(objects.get(0).getKey() + "\t" + objects.get(0).getValue());
+                if (objects.get(0).getKey().equals(objects.get(0).key) && objects.get(0) != null)
+                    if (objects.size() > 1)
+                        for (int j = 0; j < objects.size(); j++)
+                            System.out.print("-> " + objects.get(j).value + "\n");
+                }
+
+
+            }
+        }
+
     }
 
 
 
-}
 
--- src/main/java/HashTables/MainTest.java
+++ src/main/java/HashTables/MainTest.java
@@ -1,5 +1,6 @@
 package HashTables;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Objects;
 import java.util.TreeSet;
@@ -11,11 +12,16 @@ public class MainTest {
         Integer key = 16;
        ChainHash chainHash = new ChainHash();
 
-       chainHash.insertHash(12, "coffee");
+       chainHash.insertHash(32, "coffee");
        chainHash.insertHash(11, "command");
+       chainHash.insertHash(11, "hello");
+       chainHash.insertHash(11, "threat");
+       chainHash.insertHash(11, "commandos");
        chainHash.insertHash(33, "sometime");
        chainHash.insertHash(21, "forest");
        chainHash.insertHash(89, "plant");
+
+        chainHash.getAll();
 
     }
 
