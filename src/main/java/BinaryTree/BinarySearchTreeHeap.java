e913c883399dd27f8cea73ff449a5329dbd75fa4
= <pythonprogramatorqq@gmail.com>
2022-03-18 11:31:38 +0100

    Insert function for heap binary was created.

--- src/main/java/BinaryTree/BinarySearchTreeHeap.java
+++ src/main/java/BinaryTree/BinarySearchTreeHeap.java
@@ -1,4 +1,38 @@
 package BinaryTree;
 
+import java.util.*;
+
 public class BinarySearchTreeHeap {
+    int CAPACITY = 1000;
+    static int count_of_elements = 0;
+    String[] heapArray = new String[CAPACITY];
+
+    private int getRightNode(int index){
+        return index * 2 + 2;
+    }
+
+    private int getLeftNode(int index){
+        return index * 2 + 1;
+    }
+
+    private int getRootNode(int index){
+        return (index - 1) / 2;
+    }
+
+    public void insert(String key){
+
+        int i = 0;
+
+        if (count_of_elements == CAPACITY)
+            return;
+        else{
+            i = count_of_elements;
+            heapArray[i] = key;
+            count_of_elements++;
+        }
+
+
+
+    }
+
 }
